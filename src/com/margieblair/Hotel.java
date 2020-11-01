621f7677a82287401aa5258f2ebde1d2992aa696
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-01 15:06:27 -0500

    created a very basic constructor for a htoel class

---
+++ src/com/margieblair/Hotel.java
@@ -0,0 +1,14 @@
+package com.margieblair;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Hotel {
+    private String name;
+    private List<Integer> rooms;
+
+    public Hotel(String name, List<Integer> rooms) {
+        this.name = name;
+        this.rooms = rooms;
+    }
+}
